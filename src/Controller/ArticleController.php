aa470bb8c4b27729b51fe12847a5ff53d50f66aa
Slavik <spam-slavik@i.ua>
2019-11-19 01:01:03 +0200

    start symfony end

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,27 +2,55 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="article_homepage")
      */
-    public function homePage()
+    public function homepage()
     {
-        return new Response('Omg! New page :)');
+        return $this->render('homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{name}")
+     * @param $name
+     * @return Response
+     *
+     * @Route("/news/{name}", name="article_news")
      */
-    public function newsPage($name)
+    public function news($name)
     {
-        return new Response(sprintf(
-            'News page: %s',
-            $name
-        ));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/news.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $name)),
+            'comments' => $comments,
+            'name' => $name,
+        ]);
+    }
+
+    /**
+     * @param $name
+     * @param LoggerInterface $logger
+     * @return JsonResponse
+     *
+     * @Route("/news/{name}/like", name="article_news_like", methods={"POST"})
+     */
+    public function toggleLike($name, LoggerInterface $logger)
+    {
+        $logger->info('liked!');
+
+        // todo - create like, unlike
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }

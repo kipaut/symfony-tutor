7639f7365420c2d84057917f52b7150c5a89b973
Slavik <spam-slavik@i.ua>
2019-11-27 14:37:24 +0200

    start symfony config end

---
+++ src/Command/ArticleStatsCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ArticleStatsCommand extends Command
+{
+    protected static $defaultName = 'article:stats';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Return some article stats')
+            ->addArgument('name', InputArgument::REQUIRED, 'The article\'s name')
+            ->addOption('format', null, InputOption::VALUE_REQUIRED, 'The output format', 'text')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $name = $input->getArgument('name');
+
+        $data = [
+            'name' => $name,
+            'hearts' => rand(10, 100)
+        ];
+
+        switch ($input->getOption('format')) {
+            case 'text':
+                $rows = [];
+                foreach ($data as $key => $value) {
+                    $rows[] = [$key, $value];
+                }
+                $io->table(['Key', 'Value'], $rows);
+                break;
+            case 'json':
+                $io->write(json_encode($data));
+                break;
+            default:
+                throw new \Exception('Error format');
+        }
+    }
+}
